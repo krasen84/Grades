62bbb72a018d6384927c4d4dd83d57ed16d85465
krasen84 <116521221+krasen84@users.noreply.github.com>
2024-03-15 23:41:02 +0200

    fix home test 2

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -4,6 +4,6 @@ test('Check home page', async ({ page }) => {
     await page.goto('https://krasen84-grades.onrender.com/');
     const heading = await page.$('p');
     const text = await heading.textContent();
-    expect(text).toContain('Grades: 4');  
+    expect(text).toContain('Grades: 3');  
   });
   
